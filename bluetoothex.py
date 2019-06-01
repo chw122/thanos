6d1a08498b321d8b97ac6a7baaed0002cae3472e
fds5gqx <43841596+fds5gqx@users.noreply.github.com>
2019-06-01 19:21:45 +0900

    commit with database
    commit with database(mariaDB)

--- bluetoothex.py
+++ bluetoothex.py
@@ -1,11 +1,23 @@
 from bluetooth import *
+import pymysql
+
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='djawleh', db='emg_db', charset='utf8')
+
 # Create the client socket
 client_socket=BluetoothSocket( RFCOMM )
 client_socket.connect(("98:D3:31:FD:9E:A3", 1))
 while True:
-        #msg = input("Send : ")
-        #client_socket.send(msg)
-        data = client_socket.recv(1024)        
-        print ("receive : [%s]" % data)
+	#msg = input("Send : ")
+	#client_socket.send(msg)
+	data = client_socket.recv(1024)
+	data2 = client_socket.recv(1024)
+	#if(data[:1}=='/')	
+	sqlData = data.decode(encoding="utf-8") + data2.decode(encoding="utf-8")
+	print (sqlData)
+
+	with db.cursor() as cursor:
+		sql = """INSERT INTO emg_value(value, time) VALUES(""" + sqlData + """,NOW());"""
+		cursor.execute(sql)
+		db.commit()
 print ("Finished")
 client_socket.close()
