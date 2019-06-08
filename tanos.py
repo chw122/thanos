a72618aca1f2124308ee9926caa69d3af3c49d4a
chw122 <43842637+chw122@users.noreply.github.com>
2019-06-08 18:59:49 +0900

    tanos_0608

--- tanos.py
+++ tanos.py
@@ -4,8 +4,7 @@ import pyautogui
 import sys
 import time
 
-
-db = pymysql.connect(host='localhost', port=3306, user='root', passwd='12345', db='emg_db', charset='utf8')
+db = pymysql.connect(host='localhost', port=3306, user='root', passwd='djawleh', db='emg_db', charset='utf8')
 errorCode=True
 sqlData=""
 while True:
@@ -27,31 +26,34 @@ while True:
 				cursor.execute(sql)
 				db.commit()
 				
-				sql2="""SELECT value2 value1 from emg_value order by num desc limit 1"""
+				sql2="""SELECT value1 from emg_value order by num desc limit 1"""
 				cursor.execute(sql2)
 				rows = cursor.fetchall()
 				for row in rows:				
 					print(row[0])
-					print(row[1])
+				if(int(row[0])>600):					#팔을 펼때	
+					#pyautogui.click(100,100)
+					#pyautogui.press('enter')		
+					pyautogui.press('backspace')	#백스페이스 누르기
+
+				sql3="""SELECT value2 from emg_value order by num desc limit 1"""
+				cursor.execute(sql3)
+				rows = cursor.fetchall()
+				for row in rows:				
+					print(row[0])
 				
-				
-				if(int(row[1])>600)					#팔을 펼때	
-					pyautogui.click(100,100)
-					pyautogui.press('enter')		
-					#pyautogui.press('backspace')	#백스페이스 누르기
-				elif(int(row[0])>600):				#손 튕굴때
+				if(int(row[0])>700):				#손 튕굴때
 					pyautogui.click(100,100)
 					time.sleep(100)
 					pyautogui.press('enter')		#마우스좌표에 클릭
-				
+					#pyautogui.press('backspace')
 
 		else:
 			print("data error!!")
 			client_socket.close()
 			time.sleep(10)
 			break
-		
-		
+				
 		print ("insert : "+sqlData)
 
 client_socket.close()
